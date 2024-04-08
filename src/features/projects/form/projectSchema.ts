de259c3508c4f67580ce3e93f7366bb4f8e2ee38
maria bracho <mariabrachovillanueva@gmail.com>
2024-04-08 16:33:55 -0400

    fix: validate notion url

--- src/features/projects/form/projectSchema.ts
+++ src/features/projects/form/projectSchema.ts
@@ -2,9 +2,12 @@ import { z } from "zod";
 
 const REQUIRED_MESSAGE = "Este campo es requerido";
 
+const URL_NOTION_MESSAGE= "La URL debe empezar con 'https://julioquinones.notion.site'";
+const URL_NOTION_BASE = "https://julioquinones.notion.site";
+
 export const projectSchema = z.object({
   title: z.string().min(1, REQUIRED_MESSAGE),
-  url_link: z.string().min(1, REQUIRED_MESSAGE),
+  url_link: z.string().startsWith(URL_NOTION_BASE,URL_NOTION_MESSAGE).min(1, REQUIRED_MESSAGE),
   category_id: z.string().min(1, REQUIRED_MESSAGE),
   logo: z.string(),
   img: z.string(),
