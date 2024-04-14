c9263464b54d0854e113b1fc2a3b87d205925e31
maria bracho <mariabrachovillanueva@gmail.com>
2024-04-14 01:34:03 -0400

    feat: get session in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,10 +7,8 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
+  const session = await supabase.auth.getSession();
+  const user = session.data.session?.user;
   // if user is signed in and the current path is / redirect the user to /account
   if (user && req.nextUrl.pathname === "/login") {
     return NextResponse.redirect(new URL("/admin/categories", req.url));
